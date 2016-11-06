b9f2762d5374134761123b8f1086afce62f72174
robbibt <robbibt@gmail.com>
2016-11-07 09:07:23 +1100

    Add optional conversion to greyscale versions and early CMYK code

--- Scripts/image_manipulation.py
+++ Scripts/image_manipulation.py
@@ -1,27 +1,53 @@
 __author__ = 'z3287630'
 
 # Import modules
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageCms, ImageOps
 import random
-
-# File string
-file_string = 'D:/Dropbox/EarthArtAustralia/Canada/City/calgary_white_highres.png'
+import warnings; warnings.simplefilter('ignore', Image.DecompressionBombWarning)
 
 
-# Low res ------------------------------------------------------------------------------------
+# Setup and import ----------------------------------------------------------------------------------------------------
+
+file_string = 'D:/Dropbox/EarthArtAustralia/Canada/canada_roads_plasma_lowres.png'
+greyscale_convert = True
 
 # Read image
 image_highres = Image.open(file_string)
 width, height = image_highres.size
 
+
+# Desaturate and invert -----------------------------------------------------------------------------------------------
+
+if greyscale_convert:
+
+    image_black = ImageEnhance.Color(image_highres).enhance(0)
+    image_black = ImageEnhance.Contrast(image_black).enhance(1.5)
+    image_black.save(file_string[:-12] + "_black_highres.png")
+
+    # Convert to RGB if RGBA to allow invert
+    if image.mode == 'RGBA':
+
+        r,g,b,a = image_black.split()
+        rgb_image = Image.merge('RGB', (r,g,b))
+        inverted_image = ImageOps.invert(rgb_image)
+        r2,g2,b2 = inverted_image.split()
+        image_white = Image.merge('RGBA', (r2, g2, b2, a))
+        image_white.save(file_string[:-12] + "_white_highres.png")
+
+    else:
+        image_white = ImageOps.invert(image)
+        image_white.save(file_string[:-12] + "_white_highres.png")
+
+
+# Low res -------------------------------------------------------------------------------------------------------------
+
 image_lowres = image_highres.copy()
-maxsize = (2500, 3000)
+maxsize = (3000, 2500)
 image_lowres.thumbnail(maxsize, Image.ANTIALIAS)
 image_lowres.save(file_string[:-12] + "_lowres.jpg")
 
 
-
-# Frame --------------------------------------------------------------------------------------
+# Frame ---------------------------------------------------------------------------------------------------------------
 
 # Horizontal frames
 if (width > height):
@@ -97,7 +123,7 @@ elif (width < height):
     etsy_frame.save(file_string[:-12] + "_frame.jpg")
 
 
-# Subsets ------------------------------------------------------------------------------------
+# Subsets -------------------------------------------------------------------------------------------------------------
 
 # Save middle inset
 image_zoom = image_highres.crop((int(width * 0.5 - width * 0.05),
@@ -115,3 +141,23 @@ for zoom in range(2, 11):
                                      x + int(width * 0.05),
                                      y + int(height * 0.05)))
     image_zoom.save(file_string[:-12] + "_zoom_" + str(zoom) + ".jpg")
+
+
+# # CMYK conversion ---------------------------------------------------------------------------------------------------
+#
+# image_cmyk = ImageCms.profileToProfile(image_highres,
+#                                         "D:/Dropbox/EarthArtAustralia/Scripts/ICC/RGB/AdobeRGB1998.icc",
+#                                         "D:/Dropbox/EarthArtAustralia/Scripts/ICC/CMYK/USWebCoatedSWOP.icc",
+#                                         outputMode="CMYK", renderingIntent=2)
+# image_cmyk.getbands()
+# image_cmyk.save(file_string[:-12] + "_cmyk.tif",  compression="tiff_deflate")
+#
+# # Improve contrast
+# c, m, y, k = image_cmyk.split()
+# c = ImageEnhance.Contrast(c).enhance(0.9)
+# m = ImageEnhance.Contrast(m).enhance(4)
+# y = ImageEnhance.Contrast(y).enhance(3)
+#
+# image_cmyk_enhanced = Image.merge(image_cmyk.mode, (c,m,y,k))
+# image_cmyk_enhanced.save(file_string[:-12] + "_cmyk_test.tif",  compression="tiff_deflate")
+
