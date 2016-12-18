e2e491bb851ddefc55d3bcb717a1262ae25d980d
robbibt <robbibt@gmail.com>
2016-12-19 10:52:33 +1100

    Add function to calculate tags

--- Scripts/create_download_PDF.R
+++ Scripts/create_download_PDF.R
@@ -2,7 +2,7 @@ library(rmarkdown)
 
 # Set working directory and name
 setwd("D:/Dropbox/EarthArtAustralia/")
-name = "Waterways of Norway"
+name = "Shadowlands Wyoming"
 
 # Render R markdown file as PDF inside 'scripts' directory
 render("Scripts/DownloadandPrintingGuide.Rmd", "pdf_document", 
@@ -16,3 +16,20 @@ file.copy(from=from_name, to = to_name, overwrite = TRUE)
 # Remove original PDF and temporary TEX directories
 unlink("Scripts/tex2pdf*", recursive = TRUE)
 file.remove(from_name)
+
+# Create tags
+tags = function(name) {
+  
+  suffix = c("map", "art", "poster", "print", "decor", "wall art", "wall decor", "gift idea", "map print", "map art")
+  tag_list = paste(name, suffix, collapse = ', ')
+  tag_list = paste0(tag_list, ", map art, home decor")
+  
+  print(tag_list)
+  writeClipboard(tag_list)
+  
+}
+
+tags(name = "Idaho")
+tags(name = strsplit(name, split = " ")[[1]][3])
+
+
