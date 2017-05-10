13b534d96cd90c4d02fd718d8683bf08d9e1bab7
robbibt <robbibt@gmail.com>
2017-05-10 12:54:51 +1000

    Generalise create download script to allow processing for Google Drive

--- Scripts/create_download_PDF.R
+++ Scripts/create_download_PDF.R
@@ -1,16 +1,17 @@
 library(rmarkdown)
 
 # Set working directory and name
-setwd("D:/Dropbox/EarthArtAustralia/")
-name = "Streams of Arid Australia"
+setwd("D:/Google Drive/EarthArtAustralia/")
+name = "Every Road in Dallas"
 
 # Render R markdown file as PDF inside 'scripts' directory
 render("Scripts/DownloadandPrintingGuide.Rmd", "pdf_document", 
-       params = list(name = name))
+       params = list(name = name, dropbox_dir = "D:/Google Drive/EarthArtAustralia"))
 
 # Append map name to file name and copy to PDF download directory
 from_name = "Scripts/DownloadandPrintingGuide.pdf"
-to_name = paste0("Download links/DownloadandPrintingGuide-", gsub(" ", "", name, fixed = TRUE), ".pdf")
+to_name = paste0("Download links/DownloadandPrintingGuide-", 
+                 gsub(" ", "", name, fixed = TRUE), ".pdf")
 file.copy(from=from_name, to = to_name, overwrite = TRUE)
 
 # Remove original PDF and temporary TEX directories
@@ -18,21 +19,19 @@ unlink("Scripts/tex2pdf*", recursive = TRUE)
 file.remove(from_name)
 
 # Open resulting file
-shell.exec(paste0(getwd(), "/", to_name))
+shell.exec(to_name)
 
 # Create tags
-tags = function(name) {
+etsy_tags = function(name) {
   
-  suffix = c("map", "art", "poster")
+  suffix = c("map", "art", "poster", "print", "gift idea")
   tag_list = paste(name, suffix, collapse = ', ')
-  tag_list = paste0(tag_list, ", home decor, map art, unique gift, map print, wall art, art print, gift idea")
+  tag_list = paste0(tag_list, ", printable, map art, map print, wall art, art print")
   
   print(tag_list)
   writeClipboard(tag_list)
   
 }
 
-tags(name = "North Carolina")
-tags(name = strsplit(name, split = " ")[[1]][3])
-
-
+etsy_tags(name = "Germany")
+etsy_tags(name = tail(strsplit(name, split = " ")[[1]], 1))
