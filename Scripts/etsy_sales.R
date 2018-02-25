9358e8d3665290776215a0a782ba852f29aa9e5a
Robbi Bishop-Taylor <robbibt@gmail.com>
2018-02-25 15:51:13 +1100

    Fix mess made by git pull and merge

--- Scripts/etsy_sales.R
+++ Scripts/etsy_sales.R
@@ -1,24 +1,16 @@
 
-<<<<<<< Updated upstream
-=======
 library(rvest)
->>>>>>> Stashed changes
 library(tidyverse)
 library(zoo)
 library(readxl)
 library(lubridate)
 library(lucr)
-<<<<<<< Updated upstream
-
-setwd("D:/Google Drive/EarthArtAustralia/")
-=======
 library(ggthemes)
 
 setwd("C:/Users/Robbi/Google Drive/EarthArtAustralia")
 
 # Owed for conservation: 
 # sum(combined_df[579:636, "profit"])  # from Nov 17 2017 to Jan 17 2018 = 4618*0.1
->>>>>>> Stashed changes
 
 
 # Etsy sales ----------------------------------------------------------------------------
@@ -37,14 +29,11 @@ soldorders_df = list.files(path = "Donations and costs/",
   mutate(date = as.Date(`Sale Date`, format = "%m/%d/%y")) %>% 
   arrange(date) %>% 
   
-<<<<<<< Updated upstream
-=======
   # Fix adjusted amount
   mutate(`Order Net` = ifelse(!is.na(`Adjusted Net Order Amount`) & 
                               `Adjusted Net Order Amount` > 0,
                               `Adjusted Net Order Amount`, `Order Net`)) %>% 
   
->>>>>>> Stashed changes
   # Add missing dates and summarise sales/revenue/profit by date
   complete(date = full_seq(date, 1)) %>%
   group_by(date) %>% 
@@ -94,12 +83,9 @@ statements_df = list.files(path = "Donations and costs/",
   mutate(id=1:n()) %>%
   select(id, date, Activity, Fees) %>% 
   
-<<<<<<< Updated upstream
-=======
   # Coarse adjustment for USD
   mutate(Fees = Fees * 1.32) %>% 
   
->>>>>>> Stashed changes
   # Convert to long format
   group_by(date) %>%
   spread(key=Activity, value=Fees, fill = 0) %>% 
@@ -118,12 +104,9 @@ statements_df = list.files(path = "Donations and costs/",
 printful_df = list.files(path = "Donations and costs/",
                            pattern ="printful*",
                            full.names = TRUE) %>% 
-<<<<<<< Updated upstream
-=======
 
   # Remove false results
   grep(pattern ="~\\$", ., value = TRUE, invert = TRUE) %>% 
->>>>>>> Stashed changes
   
   # Read in each individually and merge output
   map(read_excel, sheet = 2, skip = 2) %>%
@@ -137,14 +120,9 @@ printful_df = list.files(path = "Donations and costs/",
   mutate(date = as.Date(Date, format = "%B %d, %Y")) %>% 
   arrange(date) %>% 
   
-<<<<<<< Updated upstream
-  # Convert string to value
-  mutate(cost = as.numeric(gsub(pattern = "\\$", replacement="", x = Total))) %>% 
-=======
   # Convert string to value and add 3% currency conversion fee
   mutate(cost = as.numeric(gsub(pattern = "\\$", replacement="", x = Total)),
          cost = cost * 1.03) %>% 
->>>>>>> Stashed changes
 
   # Select
   select(date, cost) %>% 
@@ -188,22 +166,15 @@ combined_df = left_join(soldorders_df, statements_df) %>%
          revenue_30day = rollapplyr(revenue, 30, sum, na.rm = TRUE, partial=TRUE),
          expenses_30day = rollapplyr(expenses, 30, sum, na.rm = TRUE, partial=TRUE),
          
-<<<<<<< Updated upstream
-=======
          # Rolling yearly stats
          revenue_1year = rollapplyr(revenue, 365, sum, na.rm = TRUE, partial=TRUE),
          profit_1year = rollapplyr(profit, 365, sum, na.rm = TRUE, partial=TRUE),
          expenses_1year = rollapplyr(expenses, 365, sum, na.rm = TRUE, partial=TRUE),
          
->>>>>>> Stashed changes
          # Derived 30 day stats
          profit_30day = revenue_30day - expenses_30day,
          ratioprofitrev_30day = profit_30day / revenue_30day,
          profitpersale_30day = profit_30day / sales_30day,
-<<<<<<< Updated upstream
-         salesperorder_30day = sales_30day / orders_30day,
-         yearlyequiv_30day = 365.25 * (profit_30day / 30))
-=======
          profitperorder_30day = profit_30day / orders_30day,
          revenueperorder_30day = revenue_30day / orders_30day,
          salesperorder_30day = sales_30day / orders_30day,
@@ -212,17 +183,12 @@ combined_df = left_join(soldorders_df, statements_df) %>%
          revenue_annualised = 365.25 * (revenue_30day / 30),
          expenses_annualised = 365.25 * (expenses_30day / 30),
          profit_annualised = 365.25 * (profit_30day / 30))
->>>>>>> Stashed changes
 
 
 
 # Plot -------------------------------------------------------------------------------
 
-<<<<<<< Updated upstream
-# Summary stats
-=======
 # Summary stats all time
->>>>>>> Stashed changes
 combined_df %>% 
   summarise(orders = sum(orders),
             sales = sum(sales),
@@ -230,84 +196,6 @@ combined_df %>%
             expenses = sum(expenses),
             profit = sum(profit))
 
-<<<<<<< Updated upstream
-# Last 30 days
-# Profit by day of week
-combined_df %>% 
-  tail(n=30) %>% 
-  ggplot() + geom_bar(aes(x = date, y = profit), stat = "identity") + theme_minimal() +
-  scale_x_date("Date", date_breaks = "1 day", date_labels = "%d") 
-
-
-# Last 30 days
-# Sales by day of week
-combined_df %>% 
-  tail(n=30) %>% 
-  ggplot() + geom_bar(aes(x = date, y = orders), stat = "identity") + theme_minimal() +
-  scale_x_date("Date", date_breaks = "1 day", date_labels = "%d") 
-
-
-
-# Yearly equivelent
-combined_df %>% 
-  mutate(yearlyequiv_ave = mean(yearlyequiv_30day)) %>% 
-ggplot() + 
-  geom_line(aes(x=date, y=yearlyequiv_30day)) + theme_minimal() +
-  geom_hline(aes(yintercept = yearlyequiv_ave), color = "red") +
-  geom_text(aes(y = yearlyequiv_ave*1.1, x = as.Date("2016-5-01"), 
-                label = paste0("$", round(yearlyequiv_ave, -2))), color = "red", size = 3.5) +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month") +
-  scale_y_continuous(labels = scales::dollar, breaks = seq(0, 100000, 5000))
-
-# Revenue and expenses
-ggplot(data = combined_df) + 
-  geom_line(aes(x=date, y=revenue_30day)) + 
-  geom_line(aes(x=date, y=expenses_30day), color ="red") + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month") +
-  scale_y_continuous(labels = scales::dollar, breaks = seq(0, 100000, 1000))  
-
-# Ratio of profit to revenue
-ggplot(data = combined_df) + 
-  geom_line(aes(x=date, y=ratioprofitrev_30day)) + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month") +
-  coord_cartesian(ylim = c(0, 1)) 
-
-# Profits per sale
-ggplot(data = combined_df) + 
-  geom_line(aes(x=date, y=profitpersale_30day)) + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month") +
-  scale_y_continuous(labels = scales::dollar, breaks = seq(-10, 50, 1)) +
-  coord_cartesian(ylim = c(0, max(combined_df$profitpersale_30day))) 
-
-# Sales per order
-ggplot(data = combined_df) + 
-  geom_line(aes(x=date, y=salesperorder_30day)) + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month")  
-
-
-
-# Year by year, profit
-combined_df %>% 
-  mutate(date = as.Date(paste("2012", month(date),mday(date), sep = "-"))) %>% 
-  ggplot() + 
-    geom_line(aes(x=date, y=profit_30day, color = year, group = year)) + theme_minimal() +
-    scale_x_date("Date", date_labels = "%b", date_breaks = "1 month") +
-    scale_y_continuous(labels = scales::dollar, breaks = seq(0, 100000, 1000))  
-
-# Year by year, sales
-combined_df %>% 
-  mutate(date = as.Date(paste("2012", month(date), mday(date), sep = "-"))) %>% 
-  ggplot() + 
-  geom_line(aes(x=date, y=sales_30day, color = year, group = year)) + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b", date_breaks = "1 month")
-
-# Year by year, sales per order
-combined_df %>% 
-  mutate(date = as.Date(paste("2012", month(date), mday(date), sep = "-"))) %>% 
-  ggplot() + 
-  geom_line(aes(x=date, y=salesperorder_30day, color = year, group = year)) + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b", date_breaks = "1 month")
-=======
 # Summary stats last 30 days
 combined_df %>% 
   tail(n=30) %>% 
@@ -548,24 +436,11 @@ combined_df %>%
   theme_tufte(base_family = "sans") 
 
 
->>>>>>> Stashed changes
 
 
 
 # Profit by day of week
 combined_df %>% 
-<<<<<<< Updated upstream
-  group_by(day) %>% 
-  summarise(profit = quantile(profit, 0.75)) %>% 
-  ggplot() + geom_bar(aes(x = day, y = profit), stat = "identity") + theme_minimal() +
-  scale_y_continuous(labels = scales::dollar, breaks = seq(0, 100, 10))  
-
-# Sales by day of week
-combined_df %>% 
-  group_by(day) %>% 
-  summarise(sales = quantile(sales, 0.75)) %>% 
-  ggplot() + geom_bar(aes(x = day, y = sales), stat = "identity") + theme_minimal() 
-=======
   group_by(year, day) %>% 
   summarise(profit = quantile(profit, 0.75)) %>% 
   ggplot() + geom_bar(aes(x = day, y = profit), stat = "identity") +
@@ -580,7 +455,6 @@ combined_df %>%
   ggplot() + geom_bar(aes(x = day, y = sales), stat = "identity") + 
   facet_wrap("year", ncol = 1) + 
   theme_tufte(base_family = "sans")
->>>>>>> Stashed changes
 
 
 
@@ -593,35 +467,6 @@ cost_df = combined_df %>%
   select(date, `auto-renew sold`:GST, listing, `Promoted Listings`:`renew expired`,  transaction, printful_cost) %>% 
   gather(cost, value, -date) %>% 
   mutate(value = ifelse(is.na(value), 0, value), 
-<<<<<<< Updated upstream
-         cost = factor(cost, levels =rev(c("renew expired", 
-                                           "Google Shopping Ads", 
-                                           "renew", 
-                                           "listing", 
-                                           "GST", 
-                                           "auto-renew sold", 
-                                           "transaction", 
-                                           "Promoted Listings", 
-                                           "printful_cost")))) %>% 
-  group_by(cost) %>% 
-  arrange(date) %>% 
-  mutate(value = rollapplyr(value, 30, sum, na.rm = TRUE, partial=TRUE))
-
-# Cost stacked
-ggplot(data = cost_df, aes(x = date, y = value)) +
-  geom_area(aes(colour = cost, fill= cost), position = 'stack') +
-  scale_fill_brewer(palette = "Paired") +
-  scale_color_brewer(palette = "Paired") + theme_minimal() +
-  scale_y_continuous(labels = scales::dollar, breaks = seq(0, 10000, 500)) +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month")
-
-# Cost percent
-ggplot(data = cost_df, aes(x = date, y = value)) +
-  geom_area(aes(colour = cost, fill= cost), position = 'fill') +
-  scale_fill_brewer(palette = "Paired") +
-  scale_color_brewer(palette = "Paired") + theme_minimal() +
-  scale_x_date("Date", date_labels = "%b\n%y", date_breaks = "1 month")
-=======
          cost = factor(cost, levels = rev(c("renew expired", 
                                             "Google Shopping Ads", 
                                             "renew", 
@@ -931,6 +776,5 @@ for (i in 47:length(test)) {
 
 
 
->>>>>>> Stashed changes
 
 
